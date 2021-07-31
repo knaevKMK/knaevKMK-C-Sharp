acd896cd5ba8635d905ff06dd9391d2316bcdb07
knaevKMK <kneav@students.softuni.bg>
2021-07-31 11:41:54 +0300

    practice

---
+++ advanced/adv/01 Stacks and Queues/lab/Stacks And Queues/Stack Sum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" " , StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            var stack = new Stack<int>(nums);
+           
+            string [] read = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (!read[0].ToLower().Equals("end"))
+            {
+                switch (read[0].ToLower())
+                {
+                    case "add":
+                        for (int i = 1; i < read.Length; i++)
+                        {
+                            stack.Push(int.Parse(read[i]));
+                        }
+                        break;
+                    case "remove":
+                        int count = int.Parse(read[1]);
+                        for (int i = 0; i < count; i++)
+                        {
+                            if (stack.Count == 0)
+                            {
+                                break;
+                            }
+                            stack.Pop();
+                        }
+                        break;
+                 
+                }
+                read = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            int sum = 0;
+           
+            while (stack.Count >0)
+            {
+                sum += stack.Pop();
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
---
+++ advanced/adv/01 Stacks and Queues/lab/Stacks And Queues/Stacks And Queues/Reverse Stack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_And_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string read = Console.ReadLine();
+            var stack = new Stack<char>();
+            foreach (var ch in read)
+            {
+                stack.Push(ch);
+            }
+            while (stack.Count > 0) {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
