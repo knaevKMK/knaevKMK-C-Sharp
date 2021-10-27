d40470f61c080969af8bb9dc5fdd9c100c54753e
knaevKMK <kneav@students.softuni.bg>
2021-10-27 23:11:08 +0300

    Controller_test create

---
+++ web/Advanced/Angular/evn_ex/server/evnServer/test/DepartmentControllerTest.cs
@@ -0,0 +1,34 @@
+﻿
+namespace test
+{
+    using evnServer.Controllers;
+    using evnServer.Model.View;
+    using evnServer.Service;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using NUnit.Framework;
+    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    class DepartmentControllerTest
+    {
+
+        DepartmentController controller;
+        [SetUp]
+        public void Setup() {
+            var mockService = new Mock<IDepartmentService>();
+            controller = new DepartmentController(mockService.Object);
+        }
+
+        [Test]
+        public void Ctr_Get_All()
+        {
+            var result = controller.All();
+            Assert.IsAssignableFrom(typeof(Task<ActionResult<List<DepartmentViewModel>>>),result);
+            Assert.IsAssignableFrom(typeof(ActionResult<List<DepartmentViewModel>>),result.Result);
+            Assert.IsAssignableFrom(typeof(OkObjectResult),result.Result.Result);
+        }
+    }
+}
